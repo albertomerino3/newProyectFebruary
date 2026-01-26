a9b294dcab9a4d2293735a4de4586c9e0c6b6afe
albertomerino3 <albertocampoymerino@gmail.com>
2026-01-26 14:17:51 +0100

    subo diagrama y archivo

---
+++ cuentasBancarias/src/cuentasbancarias/cuenta.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cuentasbancarias;
+import.cliente.java;
+
+/**
+ *
+ * @author EAG
+ */
+public class cuenta {
+    int num_iden;
+    double saldo_dispo;
+    Cliente duenioCuenta;
+}
